8c6691b9b8d93ac3d4f1fa8a04e3bb31f5fb2894
Christoph Reiter <reiter.christoph@gmail.com>
2022-02-21 11:28:19 +0100

    Use phpunit directly

--- src/Service/DiplomaProviderInterface.php
+++ src/Service/DiplomaProviderInterface.php
@@ -14,5 +14,6 @@ interface DiplomaProviderInterface extends LoggerAwareInterface
     public function getDiplomas(): array;
 
     public function getVerifiableCredential(Diploma $diploma, string $did): string;
+
     public function verifyVerifiableCredential($text): ?Diploma;
 }
